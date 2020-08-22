c657c853a9a5b44e00e2036ff38a0d28c9a5b15d
altaygileren <altaygileren@gmail.com>
2020-08-22 02:08:21 -0400

    Updates

---
+++ precache-manifest.d59e91e15b1aee990634d10114a766fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff50f00081489fea2c3f80fdda3036f3",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "c49fdbf7c8657bc691ca",
+    "url": "/hangman/static/css/main.c4c4fb15.chunk.css"
+  },
+  {
+    "revision": "ed5e4cbdd67764c84b19",
+    "url": "/hangman/static/js/2.44d02451.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hangman/static/js/2.44d02451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49fdbf7c8657bc691ca",
+    "url": "/hangman/static/js/main.0327dd7d.chunk.js"
+  },
+  {
+    "revision": "c94e961e9741b67a136d",
+    "url": "/hangman/static/js/runtime-main.10540b78.js"
+  }
+]);
